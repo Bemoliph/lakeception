30f5c24b35e9264f023f144c8094b8f02b0cfe71
Bemoliph <bemoliph@gmail.com>
2015-05-19 11:18:37 -0400

    Adds wrapping of world representation.

--- game.py
+++ game.py
@@ -4,14 +4,12 @@ from time import sleep
 
 class Game(object):
 	def __init__(self):
-		self.world = World("Test World", (5,5), debug=False)
+		self.world = World("Test World", (5,5), debug=True)
 		self.screen = Screen(self.world)
 	
 	def tick(self):
-		self.screen.draw((1,1), (3,3))
+		self.screen.draw((0,0), (12,12))
 
 if __name__ == "__main__":
-    g = Game()
-    while True:
-        g.tick()
-        sleep(0.85)
+	g = Game()
+	g.tick()
--- world.py
+++ world.py
@@ -16,8 +16,8 @@ class World(object):
 		
 		# Flood the world with water!
 		# All elements are references to the same water tile to save memory.
-		width, height = self.dimensions
-		tiles = [waterTile] * (width * height)
+		worldWidth, worldHeight = self.dimensions
+		tiles = [waterTile] * (worldWidth * worldHeight)
 		
 		return tiles
 	
@@ -40,17 +40,17 @@ class World(object):
 	
 	def _pointToIndex(self, point):
 		x, y = point
-		width = self.dimensions[0]
+		worldWidth = self.dimensions[0]
 		
-		index = y * width + x
+		index = y * worldWidth + x
 		
 		return index
 	
 	def _indexToPoint(self, index):
-		width = self.dimensions[0]
+		worldWidth = self.dimensions[0]
 		
-		x = i % width
-		y = i // width
+		x = i % worldWidth
+		y = i // worldWidth
 		
 		return (x, y)
 	
@@ -60,16 +60,36 @@ class World(object):
 		return self.tiles[index]
 	
 	def getTilesInArea(self, topLeft, bottomRight):
+		worldWidth = self.dimensions[0]
+		
 		x1, y1 = topLeft
 		x2, y2 = bottomRight
 		
-		width = x2-x1+1
-		height = y2-y1+1
+		areaWidth = x2-x1+1
+		areaHeight = y2-y1+1
 		
 		areaTiles = []
-		for row in xrange(0, height):
-			rowIndex = self._pointToIndex( (x1, y1+row) )
-			rowTiles = self.tiles[rowIndex : rowIndex+width]
+		for row in xrange(0, areaHeight):
+			# Add "actual" tiles to row
+			row = row % self.dimensions[1] # vertically wrap if row out of bounds
+			rowStart = self._pointToIndex( (x1, y1+row) )
+			rowEnd = (row + 1) * worldWidth
+			rowTiles = self.tiles[rowStart : rowEnd]
+			
+			# Add horizontal repeats as necessary to support visual world looping
+			# Actual + Full Width Repeats + Tail End Repeat
+			# [3 4 5]+[1 2 3 4 5][1 2 3 4 5]+[1 2 3]
+			repeatWidth = areaWidth - len(rowTiles)
+			if repeatWidth:
+				# Add any full width repeats
+				repeatCount = repeatWidth // worldWidth
+				repeatStart = row * worldWidth
+				rowTiles.extend(self.tiles[repeatStart : repeatStart+worldWidth] * repeatCount)
+				
+				# Add any "tail end" repeat
+				tailWidth = repeatWidth % worldWidth
+				rowTiles.extend(self.tiles[repeatStart : repeatStart+tailWidth])
+			
 			areaTiles.extend(rowTiles)
 		
 		return areaTiles
@@ -93,4 +113,4 @@ class Tile(object):
 
 if __name__ == "__main__":
 	w = World("Test World", (5,5), debug=True)
-	print w.getTilesInArea((0,0), (4,4))
+	print w.getTilesInArea((0,0), (4,3))

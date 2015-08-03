9080834842235e1b35d26291a8136a874f943bee
Nader Morshed <morshed.nader@gmail.com>
2015-08-03 13:29:12 -0400

    Added missing import

--- lakeception/entity.py
+++ lakeception/entity.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from lakeception import tiles
+
+
 class Entity(object):
     pass
+
 
 class Player(Entity):
     def __init__(self, pos, glyph, tileColor):
         self.pos = pos
-        self.tile = Tile("player", "the boat", glyph, tileColor)
+        self.tile = tiles.Tile("player", "the boat", glyph, tileColor)
         self.tile.elevation = "@" # hack
         self.tile.biomeID = "@" # hack

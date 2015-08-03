e451ec54adaa8d50b171f38f90963b0a52b6e194
Nader Morshed <morshed.nader@gmail.com>
2015-08-03 12:38:12 -0400

    Added argument parsing for debug / testing

--- lakeception/game.py
+++ lakeception/game.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import argparse
 import logging
 import pygame
 import sys
@@ -84,8 +85,46 @@ class Game(object):
         timeDelta = self.clock.tick(self.fps)
 
 
+def _parse_args(args):
+    """
+    Parses arguments from a argv format.
+
+    Parameters
+    ----------
+    args : list of str
+
+    Returns
+    -------
+    argparse.ArgumentParser
+    """
+    parser = argparse.ArgumentParser(
+        prog="lakeception",
+        description="Aww yeah, boats!",
+    )nt(
+        "-v", "--verbose",
+        action="store_true",
+        help="Increase verbosity of output.",
+    )
+    parser.add_argumet(
+        "--debug",
+        action="store_true",
+        help="Turn on debugging mode.",
+    )
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="Test the client lauches without issue."
+    )
+    return parser.parse_args(args)
+
+
 def main(args):
-    g = Game()
+    args = _parse_args(args)
+    g = Game(debug=args.debug)
+
+    if args.test:
+        return
+
     while not g.quitting:
         g.tick()
 

d05a66fb6e6bf1b715f5c6184a88b0eb3a4690d4
Nader Morshed <morshed.nader@gmail.com>
2015-08-03 11:14:51 -0400

    Added audio module, included function for listing audio files Updated game.py to use audio module Moved __name__ == "__main__" code into separate function Updated formatting of game.py

--- lakeception/game.py
+++ lakeception/game.py
@@ -1,34 +1,36 @@
 # -*- coding: utf-8 -*-
+
 import pygame
 import sys
-import os
 
-from world import World
-from screen import Screen
-from input import Input
+from lakeception.world import World
+from lakeception.screen import Screen
+from lakeception.input import Input
+from lakeception import audio
+
 
 class Game(object):
     def __init__(self):
         pygame.mixer.pre_init(44100, 16, 2, 4096) # setup mixer to avoid sound lag
         pygame.init()
         pygame.display.set_caption("lakeception")
-        
+
         self.world = World("Test World", (100,100), debug=False)
                                        # resolution, viewport
         self.screen = Screen(self.world, (800, 475), (25, 11))
         self.screen = Screen(self.world, (800, 475), (99, 99)) # debug res
 
         self.input = Input(self)
-        
+
         self.fps = 60
         self.clock = pygame.time.Clock()
-        
+
         self.ANIMATE = pygame.USEREVENT+0
         self.animationRate = 1 * 1000
         # Temporarily (?) disabled animations, because moving + animating was
         # an INTENSE visual experience
         # pygame.time.set_timer(self.ANIMATE, self.animationRate)
-        
+
         self.updated = True
         self.quitting = False
         self.muted = False
@@ -38,17 +40,21 @@ class Game(object):
         pygame.mixer.init()
         # From https://www.freesound.org/people/juskiddink/sounds/60507/
         # albeit a bit mixed to allow for looping
-        tracks = []
-        tracks.append(pygame.mixer.Sound(os.path.join(sys.path[0],'60507__juskiddink__waves2.ogg')))
-        tracks.append(pygame.mixer.Sound(os.path.join(sys.path[0],'sea-space.ogg')))
+        tracks = [
+            pygame.mixer.Sound(path)
+            for path in audio.get_audio_files()
+        ]
+
         # Play & loop crashing waves in the background
         # Set the volume to an unobtrusive level
         tracks[0].set_volume(0.1)
         tracks[0].play(-1)
+
         # Play the song at a slightly higher volume
         # tracks[1].set_volume(0.4)
         # tracks[1].play()
- 
+
+
     def tick(self):
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -58,14 +64,19 @@ class Game(object):
                 self.updated = False
             elif event.type == pygame.QUIT:
                 self.quitting = True
-        
+
         if self.updated:
             self.screen.draw(self.inspecting, self.editing)
             self.updated = False
-        
+
         timeDelta = self.clock.tick(self.fps)
 
-if __name__ == "__main__":
+
+def main(args):
     g = Game()
     while not g.quitting:
         g.tick()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])

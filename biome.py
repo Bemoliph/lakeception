7a7421204e3d8463e1085106058e7258d5589327
Nader Morshed <morshed.nader@gmail.com>
2015-08-03 09:33:50 -0400

    Fixed "python biome.py" not passing id to Biome()

--- biome.py
+++ biome.py
@@ -10,18 +10,18 @@ from tiles import Tile
 class Biome(object):
     def __init__(self, biomeFileName, biomeID):
         biomeFile = json.load(open(biomeFileName, "r"))
-        
+
         # Decorative name for biome
         self.name = biomeFile["name"]
         # Id which is used to reference biome in world.py
         self.id = biomeID
-        
+
         # Initialize tiles
         self.tiles = {}
         for tileName, tileData in biomeFile["tiles"].iteritems():
             self.tiles[tileName] = Tile(tileName, **tileData)
 
-        # For each initialized tile, update its list of adjacentTiles 
+        # For each initialized tile, update its list of adjacentTiles
         # so that it mirrors the now initialized tiles
         for key, tile in self.tiles.iteritems():
             if tile.adjacentTiles:
@@ -29,7 +29,7 @@ class Biome(object):
                 for tileName in tile.adjacentTiles:
                     adjacentList.append(self.tiles[tileName])
                 tile.adjacentTiles = adjacentList
-            
+
         # Set up how tiles occur at different elevations
         self.elevations = {}
         for elevation, weightedTiles in biomeFile["elevations"].iteritems():
@@ -37,22 +37,22 @@ class Biome(object):
                 elevation = int(elevation)
             except ValueError:
                 pass
-            
+
             self.elevations[elevation] = []
             for tileName, occuranceWeight in weightedTiles:
                 tile = self.tiles[tileName]
                 self.elevations[elevation].append( (tile, occuranceWeight) )
-            
+
             # Pre-sort in descending weight order.  Significantly speeds up the
             # weighted random choice algorithm used in getTileAtElevation().
             self.elevations[elevation].sort(key=operator.itemgetter(1), reverse=True)
-    
+
     def getTileAtElevation(self, elevation):
         weightedTiles = self.elevations.get(elevation, self.elevations["defaultTiles"])
-        
+
         weights = [x[1] for x in weightedTiles]
         choiceIndex = getWeightedRandomChoice(weights)
-        
+
         tile = weightedTiles[choiceIndex][0]
         # Set up some niceties, for the different debug views on the function keys
         tile.elevation = elevation
@@ -66,22 +66,22 @@ if __name__ == "__main__":
     biomes = {}
     for f in files:
         print f
-        b = Biome(f)
+        b = Biome(f, f + "_id")
         biomes[b.id] = b
 
     for biomeID in biomes:
         b = biomes[biomeID]
         print "Biome Name:", b.name
         print "Biome ID:", b.id
-        
+
         print "Tiles:"
         for tile in b.tiles.values():
             print "  <Tile: %s, \"%s\", \"%s\", %s, col=%s, \"%s\">" % (tile.glyph, tile.name, tile.allGlyphs, tile.color, tile.collidable, tile.description)
-        
+
         print "Elevations:"
         for elevation, weightedTiles in sorted(b.elevations.iteritems()):
             print "  %s: %s" % (elevation, weightedTiles)
-        
+
         print "Pick some random weighted tiles at elevations:"
         import random
         for elevation in xrange(0, 20+1):

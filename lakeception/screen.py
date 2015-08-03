30cf49ee3e4de476d11d9b66f5877b06ed456978
Nader Morshed <morshed.nader@gmail.com>
2015-08-03 14:21:18 -0400

    Use absolute imports

--- lakeception/screen.py
+++ lakeception/screen.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import pygame
 
-from lakeutils import hex2rgb, hex2rgba, rgb2hex
-from tiles import Tile
+from lakeception.lakeutils import hex2rgb, hex2rgba, rgb2hex
+from lakeception.tiles import Tile
+
 
 class Screen(object):
     def __init__(self, world, windowRes, viewportRes):
@@ -10,42 +11,42 @@ class Screen(object):
 
         self.backgroundColor = hex2rgb("0E0E0E")
         self.window = pygame.display.set_mode(windowRes)
-        
+
         self.viewportRes = viewportRes
         # viewportRes has to be square & odd for viewportCenter to be correct
         self.viewportCenter = (viewportRes[0] // 2, viewportRes[1] // 2)
         self.viewportTiles = [None] * (viewportRes[0] * viewportRes[1])
-        
+
         self.DRAWMODE_NORMAL = 0
         self.DRAWMODE_ELEVATION = 1
         self.DRAWMODE_BIOMES = 2
         self.currentDrawMode = self.DRAWMODE_NORMAL
-        
+
         self.sprites = {}
         self.fontFace = "monospace"
         self.fontSize = windowRes[0] / self.viewportRes[0]
         self.font = pygame.font.SysFont(self.fontFace, self.fontSize)
         self.fontMini = pygame.font.SysFont(self.fontFace, self.fontSize/2)
         self.fontDimensions = self.font.size("@")
-        
+
         self.cursor = (0, 0)
         self.cursorSurface = pygame.Surface((self.fontSize, self.fontSize), pygame.SRCALPHA, 32)
         self.cursorSurface.fill(hex2rgba("F2F2F2", 50))
-        
+
         self.descriptionFont = pygame.font.SysFont("monospace", 15)
-    
+
     def getFontRect(self, coords, text):
         # Scalars were experimentally determined so characters touch instead
         # of overlap/gap.  Seems fontDimensions work for big and small font sizes.
         width, height = self.fontDimensions
         height *= 0.85
         width *= 1.75
-        
+
         x = coords[0] * width
         y = coords[1] * height
-        
+
         return pygame.Rect((x, y), (width, height))
-    
+
     def _getTileGlyphForCurrentDrawMode(self, tile):
         if self.currentDrawMode == self.DRAWMODE_NORMAL:
             return (unicode(tile.glyph), tile.color)
@@ -53,39 +54,39 @@ class Screen(object):
             return (unicode(tile.elevation), tile.color)
         elif self.currentDrawMode == self.DRAWMODE_BIOMES:
             return (unicode(tile.biomeID), tile.color)
-    
+
     def _generateGlyphSprite(self, glyph, color):
         if self.currentDrawMode == self.DRAWMODE_NORMAL:
             self.sprites[(glyph, color)] = self.font.render(glyph, False, color, self.backgroundColor)
         else:
             self.sprites[(glyph, color)] = self.fontMini.render(glyph, False, color, self.backgroundColor)
-    
+
     def getSprite(self, tile, coords):
         coloredGlyph = self._getTileGlyphForCurrentDrawMode(tile)
-        
+
         if coloredGlyph not in self.sprites:
             self._generateGlyphSprite(*coloredGlyph)
 
         spriteRect = self.getFontRect(coords, coloredGlyph[0])
-        
+
         return self.sprites[coloredGlyph], spriteRect
-    
+
     def drawViewport(self):
         width, height = self.viewportRes
-        
+
         self.world.getTilesAroundPlayer((width, height), self.viewportTiles)
-        
+
         for y in xrange(0, height):
             for x in xrange(0, width):
                 tile = self.viewportTiles[y * width + x]
                 sprite, spriteRect = self.getSprite(tile, (x,y))
-                
+
                 self.window.blit(sprite, spriteRect)
 
     def drawPlayer(self):
         player = self.world.player
         sprite, spriteRect = self.getSprite(player.tile, self.viewportCenter)
-        
+
         self.window.blit(sprite, spriteRect)
 
     def drawInspectionCursor(self, editing):
@@ -121,7 +122,7 @@ class Screen(object):
             # Magic numbers here, but no need to fix this until we decide to
             # change the scalars in self.getFontRect()
             self.window.blit(label, (10, 350 + 20 * i))
-    
+
     def draw(self, inspecting, editing):
         self.window.fill(self.backgroundColor)
         self.drawViewport()
